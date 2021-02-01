4dbd2dedc0f3f7b213ec11ec043756fcb5a500f8
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-01 14:23:24 -0700

    reverseSting answered

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,14 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    var revString = []
+    for (var i = str.length-1; i>=0; i-- ){
+        revString.push(str[i])
+    }
+    revString = revString.join('')
+
+    return revString
+}
 
 module.exports = reverse;
