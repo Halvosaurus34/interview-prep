bff3e1f186f9a365d3b2c4aec8f81fff374da88e
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-01 20:48:27 -0700

    solved anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    var strA = stringA.replace(/[^\w]/g, "").toLowerCase()
+    var strB = stringB.replace(/[^\w]/g, "").toLowerCase()
+    var charA = {}
+    var charB = {}
+    for (var char of strA) {
+        if (!charA[char]){
+            charA[char] = 1
+        } else {
+            charA[char]++
+        }
+    }
+    for (var char of strB) {
+        if (!charB[char]){
+            charB[char] = 1
+        } else {
+            charB[char]++
+        }
+    }
+    if (strA.length === strB.length){
+        for (var keyA in charA){
+            if (!charB[keyA]){
+                return false
+            } else if (charB[keyA] !== charA[keyA]){
+                return false
+            } else {
+                console.log(charB[keyA])
+                return true
+            }
+        }
+    } else {
+        return false
+    }
+}
 
 module.exports = anagrams;
