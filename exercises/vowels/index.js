72fca760de9debe0f46e5f29dc29609d9fb22692
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-02 14:36:39 -0700

    solved vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,26 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    var cleanString = str.replace(/[^\w]/g, "").toLowerCase()
+    const checker = ['a', 'e', 'i', 'o', 'u']
+    var count = 0
+    for (var char of cleanString) {
+        if (checker.includes(char)){
+            count ++
+        } 
+    }
+    return count
+}
 
 module.exports = vowels;
+
+//original try
+// var cleanString = str.replace(/[^\w]/g, "").toLowerCase()
+//     var count = 0
+//     for (var char of cleanString) {
+//         if (char === 'a' || char === 'e' || char === 'i' || char === 'o'|| char === 'u'){
+//             count ++
+//         } 
+//     }
+//     return count
