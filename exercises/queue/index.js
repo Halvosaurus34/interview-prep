29f395bf7e660bde758f2e3e17d8f6bb386d7d4e
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-02 19:50:00 -0700

    solved queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,17 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor(){
+        this.data = []
+    }
+
+    add(n){
+        this.data.unshift(n)
+    }
+    remove(){
+        return this.data.pop()
+    }
+}
 
 module.exports = Queue;
