d2a4f0f9014a5aa451bc9e56195a85a7bacc861f
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-03 13:01:41 -0700

    solved linked list getfirst

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -28,6 +28,75 @@ class LinkedList {
         }    
         return counter
     }
+
+    getFirst(){
+        return this.head
+    }
+
+    getLast(){
+        var node = this.head
+
+        while(node.next){
+            node = node.next
+        }
+
+        return node
+    }
+
+    clear(){
+        this.head = null
+    }
+
+    removeFirst(){
+        this.head = this.head.next
+    }
+
+    removeLast(){
+        var node = this.head
+        var nextNode = null
+        if(!node){
+            return
+        } else if(!node.next){
+            this.head = null
+            return
+        } else if (node.next){
+            while(node.next){
+                nextNode = node
+                node = node.next
+                console.log(node)
+            }
+        } 
+        
+        if (nextNode){
+            nextNode.next = null
+        } else {
+            node = null
+        }
+    }
+
+    insertLast(data){
+        var newNode = new Node(data)
+        var node = this.head
+        while(node.next){
+            node = node.next
+            console.log(node)
+        }
+        node.next = newNode
+    }
+
+    getAt(n){
+        var counter = 0
+        var node = this.head
+        var nextNode = null
+
+        while(counter<=n){
+            counter++
+            nextNode = node
+            node = node.next
+            console.log(node)
+        }
+        return nextNode
+    }
 }
 
 module.exports = { Node, LinkedList };
