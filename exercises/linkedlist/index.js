1338dad20845bfc31515ea88fd273a4d6279ca5d
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-03 12:32:33 -0700

    solved linked list size

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,32 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    constructor(data, next = null){
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor(){
+        this.head = null
+    }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head)
+    }
+
+    size(){
+        var counter = 0
+        var node = this.head
+
+        while (node){
+            counter++
+            node = node.next
+        }    
+        return counter
+    }
+}
 
 module.exports = { Node, LinkedList };
