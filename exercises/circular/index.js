e1fbae63b0146e09fa921f6817551b90e43066b3
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-05 19:33:01 -0700

    solved circular

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,18 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    var slow = list.head
+    var fast = list.head
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (slow === fast) {
+            return true
+        }
+    }
+    return false
+}
 
 module.exports = circular;
