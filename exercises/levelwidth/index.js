a405b2056133e32fb7bc51128e21ae0beaa4287f
Levi Halvorson <lhalvorson34@gmail.com>
2021-02-05 21:17:28 -0700

    solved level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, "S"]
+    const counter = [0]
+
+    while(arr.length > 1){
+        const arrFirst = arr.shift()
+        if (arrFirst === "S") {
+            counter.push(0)
+            arr.push("S")
+        } else {
+            arr.push(...arrFirst.children)
+            counter[counter.length-1]++
+        }
+
+    }
+    return counter
+}
 
 module.exports = levelWidth;
